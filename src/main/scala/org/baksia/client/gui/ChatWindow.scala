c056a8fabca7ac2a4c0f07f972ead188bc1bbd25
Reidar Sollid <rsollid@gmail.com>
2013-09-26 19:52:03 +0200

    Added basic functionality

--- src/main/scala/org/baksia/client/gui/ChatWindow.scala
+++ src/main/scala/org/baksia/client/gui/ChatWindow.scala
@@ -4,14 +4,36 @@ import swing._
 import event._
 
 object ChatWindow extends SimpleSwingApplication {
+
   def top = new MainFrame {
     title = "Scakka"
-    contents = newField		
-  }
+    object sendButton extends Button {
+      text = "Send"
+    }
 
-  def newField = new TextField {
-    text = "0"
-    columns = 5
-    horizontalAlignment = Alignment.Right
+    object messagesArea extends TextArea {
+      text = "Conversation"
+      columns = 20
+      rows = 30      
+      editable = false
+    }
+    
+    object messageField extends TextField {
+      text = "Write messages here"
+      columns = 20
+      horizontalAlignment = Alignment.Right
+    }
+
+    contents = new FlowPanel {
+      contents.append(messageField, sendButton, messagesArea)
+      border = Swing.EmptyBorder(10)
+    }
+
+    listenTo(sendButton)
+    reactions += {
+      case ButtonClicked(sendButton) =>
+        println(messageField.text)
+        messagesArea.append("\n" + messageField.text)
+    }
   }
 }

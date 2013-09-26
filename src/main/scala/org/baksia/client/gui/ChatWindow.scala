12e16bb14695e763c85281f140fcc88d62fb68c6
Reidar Sollid <rsollid@gmail.com>
2013-09-26 23:01:38 +0200

    ClientHandler actor and event publisher added

--- src/main/scala/org/baksia/client/gui/ChatWindow.scala
+++ src/main/scala/org/baksia/client/gui/ChatWindow.scala
@@ -2,8 +2,15 @@ package org.baksia.client.gui
 
 import swing._
 import event._
+import akka.actor.Actor
+import akka.actor.ActorSystem
+import akka.actor.Props
+
+case class MessageEvent(message: String) extends Event
 
 object ChatWindow extends SimpleSwingApplication {
+
+  val clientHandler = ActorSystem("ClientSystem").actorOf(Props[ClientHandler])
 
   def top = new MainFrame {
     title = "Scakka"
@@ -14,10 +21,10 @@ object ChatWindow extends SimpleSwingApplication {
     object messagesArea extends TextArea {
       text = "Conversation"
       columns = 20
-      rows = 30      
+      rows = 30
       editable = false
     }
-    
+
     object messageField extends TextField {
       text = "Write messages here"
       columns = 20
@@ -33,7 +40,10 @@ object ChatWindow extends SimpleSwingApplication {
     reactions += {
       case ButtonClicked(sendButton) =>
         println(messageField.text)
+        clientHandler ! messageField.text
         messagesArea.append("\n" + messageField.text)
+      case MessageEvent(message) =>
+        messagesArea.append(message)
     }
   }
 }

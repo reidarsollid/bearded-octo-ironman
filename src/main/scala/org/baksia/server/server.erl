26cb303c1811deae12453aaebe43649bed1422f7
Reidar <rsollid@gmail.com>
2014-07-07 23:34:39 +0200

    Formated Erlang source

--- src/main/scala/org/baksia/server/server.erl
+++ src/main/scala/org/baksia/server/server.erl
@@ -1,22 +1,22 @@
--module (server).
+-module(server).
 
--export ([start/0]).
+-export([start/0]).
 
-start() -> 
-	{ok, LSock} = gen_tcp:listen(1111, [binary, {packet, 0}, {active, false}]),
-	{ok, Sock} = gen_tcp:accept(LSock),
-	{ok, Bin} = do_recv(Sock),
-	ok = gen_tcp:close(Sock),
-	Bin.
+start() ->
+  {ok, LSock} = gen_tcp:listen(1111, [binary, {packet, 0}, {active, false}]),
+  {ok, Sock} = gen_tcp:accept(LSock),
+  {ok, Bin} = do_recv(Sock),
+  ok = gen_tcp:close(Sock),
+  Bin.
 
 do_recv(Sock) ->
-	case gen_tcp:recv(Sock, 0) of
-		{ok, B} ->
-			io:format(B),
-			io:format("~n"),
-			gen_tcp:send(Sock, B),
-			do_recv(Sock);
-		{error, closed} ->
-			{ok, farewell}
-	end.
+  case gen_tcp:recv(Sock, 0) of
+    {ok, B} ->
+      io:format(B),
+      io:format("~n"),
+      gen_tcp:send(Sock, B),
+      do_recv(Sock);
+    {error, closed} ->
+      {ok, farewell}
+  end.
 	
